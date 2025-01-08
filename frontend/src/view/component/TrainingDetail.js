aa5eb920246ec125d5f504389c87661972bab230
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-08 14:52:01 +0700

    Update TrainingDetail.js

--- frontend/src/view/component/TrainingDetail.js
+++ frontend/src/view/component/TrainingDetail.js
@@ -138,13 +138,7 @@ const cardStyle = {
                                     </div>
                                 </Col>
                                 <Col xs={24} md={24}>
-                                <div>
-                                    <img
-                                            alt="avatar"
-                                            src={Config.image_path + blog.Image}
-                                            // style={imgStyle}
-                                        />
-                                </div>         
+                                        
                                 <div style={imgContainerStyle}>
                                     <img
                                         alt="avatar"
